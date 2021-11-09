e436e1b0bcc20b5fd9f8a3b3bcd3e011af419de0
SeanXiao <seanxiao@outlook.com>
2021-11-09 17:06:49 +0800

    2021-11-%

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 today=`date +%Y-%m-%`
+
+## deploy to shei5.com
 docker exec  blog sh -c "cd /blog/blog && hexo g"
 
 
-
-#cd /data/hexo/blog/public
-#git add *
-#git commit -m "$today"
-#git push -u origin main
+## git push to github.com
+cd /data/hexo/blog/public
+git add *
+git commit -m "$today"
+#git push -u origin main # -u参数是作关联 第一次使用
+git push origin main
